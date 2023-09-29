ad074526a4e3ac245799429e7d2ed37621160f90
Alfred Hernandez <alfred.t.hernandez@gmail.com>
2023-09-28 20:04:53 -0700

    Stripped base Vite, Router groundwork created with NavBar Component base

---
+++ sheDesNails/src/component/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = () =>{
+    return(
+        <div>
+            <h1>THIS IS NAVBAR H1</h1>
+            
+        </div>
+    )
+}
+
+export default NavBar
--- sheDesNails/src/main.jsx
+++ sheDesNails/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  errorElement: <h1>WRONG, TRY AGAIN</h1>,
+  children: [],
+}
+]
 )
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}/>)
